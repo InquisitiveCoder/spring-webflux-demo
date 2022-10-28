22f19decc3918059c3a330cea4bdc556a0dced27
Armando Doval <armando.doval88@gmail.com>
2022-10-28 16:49:25 -0400

    Added Swagger and README.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Running the service
+
+From the project directory, execute one of the following commands:
+#### cmd.exe (Windows)
+`gradlew.bat bootRun`
+
+#### bash (Linux, macOS)
+`./gradlew bootRun`
+
+Then navigate to http://localhost:8080/swagger-ui.html
--- src/main/java/com/doval88/weather/RouterConfiguration.java
+++ src/main/java/com/doval88/weather/RouterConfiguration.java
@@ -1,15 +1,34 @@
 package com.doval88.weather;
 
 import com.doval88.weather.dailysummary.*;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+import io.swagger.v3.oas.annotations.media.*;
+import io.swagger.v3.oas.annotations.responses.*;
+import org.springdoc.core.annotations.*;
 import org.springframework.context.annotation.*;
 import org.springframework.web.reactive.function.server.*;
 
 import static org.springframework.http.MediaType.*;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration(proxyBeanMethods = false)
+@OpenAPIDefinition(info = @Info(title = "Spring Reactive Demo", version = "1.0"))
 public class RouterConfiguration {
     @Bean
+    @RouterOperations({
+            @RouterOperation(
+                    path = "/forecast",
+                    produces = {APPLICATION_JSON_VALUE},
+                    method = GET,
+                    operation = @Operation(
+                            operationId = "getForecast",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            content = @Content(
+                                                    schema = @Schema(implementation = DailySummaryResponse.class)))}))})
     public RouterFunction<ServerResponse> route(DailySummaryHandler dailySummaryHandler) {
         return RouterFunctions.route(GET("/forecast").and(accept(APPLICATION_JSON)), dailySummaryHandler::forecast);
     }
